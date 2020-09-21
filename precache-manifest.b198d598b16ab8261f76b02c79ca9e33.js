7c3f19988176eaa19b50df7ce7e4bd2e6599b7e3
trinhxuankhanh <xuankhanhs@gmail.com>
2020-09-21 16:35:25 +0700

    Updates

---
+++ precache-manifest.b198d598b16ab8261f76b02c79ca9e33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8845997348a4696d29d6885e621183b0",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "0255a308d8e04a52809b",
+    "url": "/cv/static/css/2.f2c5df53.chunk.css"
+  },
+  {
+    "revision": "fbb842dc9166487fdd1f",
+    "url": "/cv/static/css/main.d5743eeb.chunk.css"
+  },
+  {
+    "revision": "0255a308d8e04a52809b",
+    "url": "/cv/static/js/2.ea214480.chunk.js"
+  },
+  {
+    "revision": "db4833b02f62bd470bb1579a2ec8a284",
+    "url": "/cv/static/js/2.ea214480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbb842dc9166487fdd1f",
+    "url": "/cv/static/js/main.03786b08.chunk.js"
+  },
+  {
+    "revision": "142654392df4a73f8c8e",
+    "url": "/cv/static/js/runtime-main.58abac5b.js"
+  }
+]);
